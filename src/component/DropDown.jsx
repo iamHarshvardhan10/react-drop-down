d1f2f3b00c9f1ba76ce901bc3b37a96b8e605812
Harshvardhan Bhosale <109547739+iamHarshvardhan10@users.noreply.github.com>
2024-07-10 20:17:51 +0000

    created drop down

--- src/component/DropDown.jsx
+++ src/component/DropDown.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 
-import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { IoIosArrowDropdownCircle , IoIosArrowDroprightCircle } from "react-icons/io";
 
 
 
@@ -28,16 +28,82 @@ const navlinks = [
 ]
 
 
+const products = [
+    {
+        title: 'Mobile'
+    },
+    {
+        title: 'Laptop'
+    },
+    {
+        title: 'Clothes'
+    },
+    {
+        title: 'Leds'
+    },
+    {
+        title: 'Car'
+    }
+]
+
+const clothes = [
+    {
+        title: 'T-shirt'
+    },
+    {
+        title: 'Shirt'
+    }
+]
+
 
 const DropDown = () => {
     return (
         <div className='w-[100%]'>
             <div className='flex items-center justify-center gap-[10rem] my-[20vh] mx-[300px] border p-4 rounded-xl'>
                 {
-                    navlinks.map((navlink) => (
-                        <div key={navlink.title} className='flex items-center gap-2 text-lg capitalize'>
-                            <Link to={navlink.link}>{navlink.title}</Link>
-                            <span><IoIosArrowDropdownCircle /></span>
+                    navlinks.map((navlink, i) => (
+                        <div key={i} className='group flex items-center'>
+                            {
+                                navlink.title == 'Product' ? (
+                                    <div className='relative flex items-center gap-2 cursor-pointer group'>
+                                        <span>{navlink.title}</span>
+                                        <span><IoIosArrowDropdownCircle /></span>
+                                        <div className='invisible absolute top-[50%] w-[150px] text-lg uppercase flex flex-col gap-4 translate-x-[-20%] translate-y-[-3em] bg-[#A68DAD] font-bold p-4 rounded-lg group-hover:visible group-hover:translate-y-[1.65em] select-none transition-all duration-200 '>
+                                            {
+                                                products.map((product) => (
+                                                    <div>
+                                                        {
+                                                            product.title == 'Clothes' ? (
+                                                                <div className='relative flex items-center gap-2 group'>
+                                                                    <span>{product.title}</span>
+                                                                    <span><IoIosArrowDroprightCircle /></span>
+                                                                    <div className='invisible absolute top-[50%] left-[130%] translate-x-[-50%] bg-[#ADCCC7] w-[140px] p-4 group-hover:visible group-hover:translate-x-[-10%] transition-all duration-200 rounded-lg'>
+                                                                        {
+                                                                            clothes.map((cloth) => (
+                                                                                <div key={cloth.title} className='mb-2'>
+                                                                                    <p>{cloth.title}</p>
+                                                                                </div>
+                                                                            ))
+                                                                        }
+                                                                    </div>
+                                                                </div>
+                                                            ) : (
+                                                                <div>
+                                                                    <p>{product.title}</p>
+                                                                </div>
+                                                            )
+                                                        }
+                                                    </div>
+                                                ))
+                                            }
+                                        </div>
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <p>{navlink.title}</p>
+                                    </div>
+                                )
+                            }
                         </div>
                     ))
                 }
